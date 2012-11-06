9e159a616f6f432355d06b6778e3a09b84e168c2
Taras <tapacpro@gmail.com>
2012-11-06 17:36:04 +0600

    main <-

--- js/mysammy.js
+++ js/mysammy.js
@@ -4,35 +4,32 @@
 	//add plugin
         this.use('Mustache');
 
-	// define a 'route'
+	// define a 'main-route'
         this.get('#/', function() {
-	    // load some data
 
-            this.render('mustache/slider.mustache')
-	    // swap the DOM with the new content
-		.replace('#slider-main');
 
 	    this.load('json/mini-boxes.json')
 
-		.render('mustache/mini-boxes.mustache')
-	    // swap the DOM with the new content
-		.replace('#mini-boxes');
-
             this.render('mustache/main.mustache')
-	    // swap the DOM with the new content
+
 		.replace('#main');
 
-            this.render('mustache/footer.mustache')
-	    // swap the DOM with the new content
-		.replace('#footer');
+        });
 
 
-	});
+	this.get('#/structura', function() {
+
+            this.render('mustache/structura.mustache')
+
+		.replace('#main');
+
+        });
+
 
 
     });
 
     $(function() {
-	app.run('#/')
+	app.run()
     });
 })(jQuery);
